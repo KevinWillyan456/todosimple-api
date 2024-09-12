74f7c0afca06633f02a3c0d52c66dc9fb494f249
Kevin Souza <kevinwillyansouza@gmail.com>
2024-09-12 17:08:31 -0400

    feat: criação do UserDetailsServiceImpl e JWTUtil

--- src/main/java/com/kevinsouza/todosimple/configs/SecurityConfig.java
+++ src/main/java/com/kevinsouza/todosimple/configs/SecurityConfig.java
@@ -2,23 +2,36 @@ package com.kevinsouza.todosimple.configs;
 
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import com.kevinsouza.todosimple.security.JWTUtil;
+
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+	private AuthenticationManager authenticationManager;
+
+	@Autowired
+	private UserDetailsService userDetailsService;
+
+	@Autowired
+	private JWTUtil jwtUtil;
 
 	private static final String[] PUBLIC_MATCHERS = { "/" };
 	private static final String[] PUBLIC_MATCHERS_POST = { "/user", "/login" };
@@ -27,6 +40,12 @@ public class SecurityConfig {
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
 		http.cors(cors -> cors.configurationSource(corsConfigurationSource())).csrf(csrf -> csrf.disable());
+
+		AuthenticationManagerBuilder authenticationManagerBuilder = http
+				.getSharedObject(AuthenticationManagerBuilder.class);
+		authenticationManagerBuilder.userDetailsService(this.userDetailsService)
+				.passwordEncoder(bCryptPasswordEncoder());
+		this.authenticationManager = authenticationManagerBuilder.build();
 
 		http.authorizeHttpRequests(requests -> requests.requestMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST)
 				.permitAll().requestMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated());
